2251322477abed892b2fba4bef0e5d3a673d59b1
Benny Dabush <bennydabush@gmail.com>
2023-07-09 15:47:28 +0300

    remove /

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ const corsOptions = {
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:3000",
-    "https://master--remarkable-pithivier-5a4f7e.netlify.app/",
+    "https://master--remarkable-pithivier-5a4f7e.netlify.app",
   ],
   optionsSuccessStatus: 200,
 };
