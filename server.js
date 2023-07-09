4d5e36c3494c35b748097467b64ddec912873d12
Benny Dabush <bennydabush@gmail.com>
2023-07-09 14:27:24 +0300

    get /

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(cors);
 app.use(logger);
 app.use(express.json());
 app.use(router);
-
+app.get("/", (req, res) => {
+  res.send("welcome to cards server");
+});
 app.use((err, req, res, next) => {
   handleError(res, 500, "Internal error " + err.message);
 });
