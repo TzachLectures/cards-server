b4da65f1bab8254b8480dac2d71b3880d6538727
Benny Dabush <bennydabush@gmail.com>
2023-07-09 15:45:43 +0300

    add origin to cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://127.0.0.1:5500", "http://localhost:3000"],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+    "https://master--remarkable-pithivier-5a4f7e.netlify.app/",
+  ],
   optionsSuccessStatus: 200,
 };
 
